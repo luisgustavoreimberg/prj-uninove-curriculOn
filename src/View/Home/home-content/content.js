b57a2b38bf82ffbd1899ebc3db2d30c1df52da86
Luis Reimberg <lreimberg@a5-solutions.local>
2020-06-09 22:22:25 -0300

    Colocando os valores do form em variáveis

--- src/View/Home/home-content/content.js
+++ src/View/Home/home-content/content.js
@@ -7,40 +7,53 @@ import img_info from "../../resources/images/img_info.jpg";
 import img_indeed from "../../resources/images/img_indeed_busca.jpg";
 import img_trabalhaBrasil from "../../resources/images/img_trabalhaBrasil_busca.jpg";
 
+
 const HomeContent = (props) => {
     const {
       buttonLabel,
-      className
+      className,
 } = props;
 
 const [modal, setModal] = useState(false);
+const toggle = () => {
+    setModal(!modal);
+    setVaga("");
+    setDetalhes("");
+    setSalario(0);
+    setCidade("");
+    setUf("");
+    setBtn(false);
+}
 
-const toggle = () => setModal(!modal);
 
-function infoParaEnvio(vaga,detalhes, salario, cidade, uf){
-    var txt = "0";
-    if(vaga === undefined || vaga === ""){
-        txt += "1";
-        // return false;
-    }        
-    if(detalhes === undefined || detalhes === ""){
-        txt += "2";
-        // return false;
+const [vaga, setVaga] = useState("");
+const defineVaga = ev => {setVaga(ev.target.value); checaDadosParaPesquisa()}
+
+const [detalhes, setDetalhes] = useState("");
+const defineDetalhes = ev => {setDetalhes(ev.target.value); checaDadosParaPesquisa()}
+
+const [salario, setSalario] = useState(0);
+const defineSalario = ev => {setSalario(ev.target.value); checaDadosParaPesquisa()}
+
+const [cidade, setCidade] = useState("");
+const defineCidade = ev => {setCidade(ev.target.value); checaDadosParaPesquisa()}
+
+const [uf, setUf] = useState("");
+const defineUf = ev => {setUf(ev.target.value); checaDadosParaPesquisa()}
+
+const [btnEnabled, setBtn] = useState(false);
+const checaDadosParaPesquisa = () =>{
+    if(vaga != undefined && vaga != "" &&
+    detalhes != undefined && detalhes != "" &&
+    salario >= 0 && salario <= 9999999 &&
+    cidade != undefined && cidade != "" &&
+    uf != undefined && uf != ""){
+        setBtn(true);
+        return true;
+    }else{
+        setBtn(false);
+        return false;
     }
-    if(salario<0 || salario>9999999){
-        txt += "3";
-        // return false;
-    }
-    if(cidade === undefined || cidade === ""){
-        txt += "4";
-        // return false;
-    }
-    if(uf === undefined || uf === ""){
-        txt += "5";
-        // return false;
-    }
-    // return true;
-    alert(txt);
 }
 
 return(
@@ -80,37 +93,41 @@ return(
 
 
                 <Modal isOpen={modal} toggle={toggle} className={className}>
-                    <ModalHeader toggle={toggle}>Preencha aqui suas informações</ModalHeader>
+                    <ModalHeader toggle={toggle}>
+                        Preencha aqui suas informações
+                    </ModalHeader>
+                    
                     <ModalBody>
                         <InputGroup>
-                            <InputGroupAddon addonType="prepend">VAGA</InputGroupAddon>
-                            <Input name="titulo_vaga" type="text" placeholder="Ex.: Programador"/>
+                            <InputGroupAddon addonType="prepend">VAGA*</InputGroupAddon>
+                            <Input onChange={defineVaga} name="titulo_vaga" type="text" placeholder="Ex.: Programador"/>
                         </InputGroup>
                         <br/>
                         <InputGroup>
                             <InputGroupAddon addonType="prepend">DETALHES</InputGroupAddon>
-                            <Input name="espec_vaga" type="text" placeholder="Ex.: Web React"/>
+                            <Input onChange={defineDetalhes} name="espec_vaga" type="text" placeholder="Ex.: Web React"/>
                         </InputGroup>
                         <br/>
                         <InputGroup>
-                            <InputGroupAddon name="salario_vaga" addonType="prepend" placeholder="ex.: Programador">SALÁRIO R$:</InputGroupAddon>
-                            <Input type="number"
+                            <InputGroupAddon name="salario_vaga" addonType="prepend" placeholder="ex.: Programador">SALÁRIO R$*</InputGroupAddon>
+                            <Input onChange={defineSalario} type="number"
                             min="0,00" max="9999999,00" maxLength="11" step="1.00"
                             placeholder="4.000,00"/>
                         </InputGroup>
                         <span className="txt-example">de 0 a 9999999,00</span>
                         <br/><br/>
                         <InputGroup>
-                            <InputGroupAddon addonType="prepend">CIDADE</InputGroupAddon>
-                            <Input name="cidade_vaga" type="text" placeholder="São Paulo"/>
-                            <InputGroupAddon addonType="prepend">UF</InputGroupAddon>
-                            <Input name="uf_vaga" type="text" maxLength="2" placeholder="SP"/>
+                            <InputGroupAddon addonType="prepend">CIDADE*</InputGroupAddon>
+                            <Input onChange={defineCidade} name="cidade_vaga" type="text" placeholder="São Paulo"/>
+                            <InputGroupAddon addonType="prepend">UF*</InputGroupAddon>
+                            <Input onChange={defineUf} name="uf_vaga" type="text" maxLength="2" placeholder="SP"/>
                         </InputGroup>
                         
                     </ModalBody>
                     <ModalFooter>
-                    <Button color="success" onClick={toggle}>BUSCAR MINHA VAGA</Button>{' '}
-                    <Button color="danger" onClick={toggle}>Cancel</Button>
+                    <Button disabled={!btnEnabled?'true':''} color="success" onClick={toggle}>BUSCAR MINHA VAGA</Button>{' '}
+                    <Button color="danger" onClick={toggle}>Cancelar</Button>
+                    <span className="modal-erro-preenchimento" hidden={btnEnabled?'true':''}>PREENCHA CORRETAMENTE OS DADOS OBRIGATÓRIOS(*)</span>
                     </ModalFooter>
                 </Modal>
             </div>
